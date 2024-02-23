a5f9e613b2f8c63568574453b037c84f23a0a449
corpltd <89472911+corpltd@users.noreply.github.com>
2024-02-23 16:40:46 +0100

    🟢 Variabler lagt til

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,21 @@
 terraform {
-    required_version = "~> 1.6.4"
+    required_version = ">= 1.6.4"
+}
+
+variable "docker_repo" {
+  description = "Dockerhub username"
+  type        = string
+  sensitive   = true
+}
+
+variable "docker_username" {
+  description = "Dockerhub username"
+  type        = string
+  sensitive   = true
 }
 
 resource "dockerhub_repository" "project" {
-  name        = "project-name"
-  namespace   = "your-dockerhub-username"
+  name        = var.docker_repo
+  namespace   = var.docker_username
   description = "Project description"
 }
