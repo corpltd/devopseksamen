5b949a665ec218b44f083c81a10cc466e9574626
corpltd <89472911+corpltd@users.noreply.github.com>
2024-02-23 18:03:52 +0100

    🔴 Forgot required_providers

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,12 @@
 terraform {
     required_version = ">= 1.6.4"
+
+    required_providers {
+        dockerhub = {
+            source = "BarnabyShearer/dockerhub"
+            version = ">= 0.0.15"
+        }
+    }
 }
 
 variable "docker_repo" {
